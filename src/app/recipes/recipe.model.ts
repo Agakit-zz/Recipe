bbe997f83d51820cb753016011c7968654465bbc
[Agaki Tatiana] <[agaki.tatiana@gmail.com]>
2019-03-10 19:38:50 +0200

    pushing data from A to B

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,15 @@
+import { Ingredient } from '../shared/igredient.model';
+
 export class Recipe {
   public name: string;
   public descriptioin: string;
   public imagePath: string;
+  public ingredients: Ingredient[];
 
-  constructor(name: string, desc: string, imagePath: string) {
+  constructor(name: string, desc: string, imagePath: string, ingredients: Ingredient[]) {
     this.name = name;
     this.descriptioin = desc;
     this.imagePath = imagePath;
+    this.ingredients = ingredients;
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,37 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/igredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
+  
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://requestreduce.org/images/recipe-book-cover-clipart-22.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://requestreduce.org/images/recipe-book-cover-clipart-22.jpg')
+    new Recipe(
+      'Tasty Schitzel', 
+      'A Super-Tasty Schitzel', 
+      'https://s1.eda.ru/StaticContent/Photos/130410124428/130420092928/p_O.jpg',
+      [
+        new Ingredient ('Meat', 1),
+        new Ingredient ('French Fries', 20)
+      ]),
+      new Recipe(
+        'Fat Burger', 
+        'A Super-Tasty Fat Burger', 
+        'https://bakedpizza.com.co/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/h/a/hamburguesa_doble_carne_1.png',
+        [
+          new Ingredient ('Meat', 1),
+          new Ingredient ('Buns', 2)
+        ])
   ];
-
+  
+  constructor(private slService: ShoppingListService) {}
   getRecipes() {
-    // return this.recipes.slice();
-    return this.recipes
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
